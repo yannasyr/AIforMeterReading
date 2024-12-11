4a73795ef33ac738bd6dd3a3545fdd2c363a63bb
Yann Rousseau <yann.rousseau27@gmail.com>
2024-12-11 14:24:17 +0100

    update

--- py_scripts/annotation_manuelle.py
+++ py_scripts/annotation_manuelle.py
@@ -1,31 +1,30 @@
 import cv2
 import os
-
-# Répertoires des images et annotations
-image_dir = "datasets/YOLO-training-bis/images200/"  # Répertoire contenant les images
-output_dir = "datasets/YOLO-training-bis/labels200/"  # Répertoire pour sauvegarder les annotations
+import random
+import shutil
 
 def annotate_image(image_path):
     """
     Fonction pour annoter une image.
-    L'utilisateur clique et trace un rectangle autour de la zone d'intérêt.
+    L'utilisateur clique et trace des rectangles autour des zones d'intérêt.
     """
     global roi_coords
-    roi_coords = []
+    roi_coords = []  # Liste pour stocker toutes les annotations
 
     def draw_rectangle(event, x, y, flags, param):
-        global roi_coords
+        global roi_coords, temp_image
         if event == cv2.EVENT_LBUTTONDOWN:
-            roi_coords = [(x, y)]  # Coordonnées de départ
+            roi_coords.append([(x, y)])  # Début d'un rectangle
         elif event == cv2.EVENT_LBUTTONUP:
-            roi_coords.append((x, y))  # Coordonnées de fin
+            roi_coords[-1].append((x, y))  # Fin du rectangle
             # Dessiner le rectangle
-            cv2.rectangle(temp_image, roi_coords[0], roi_coords[1], (255, 0, 0), 2)
+            cv2.rectangle(temp_image, roi_coords[-1][0], roi_coords[-1][1], (255, 0, 0), 2)
             cv2.imshow("Image", temp_image)
 
     # Charger l'image
     image = cv2.imread(image_path)
-    image = cv2.resize(image, (640,640))
+    image = cv2.resize(image, (640, 640))
+    global temp_image
     temp_image = image.copy()
     cv2.imshow("Image", image)
 
@@ -33,7 +32,8 @@ def annotate_image(image_path):
     cv2.setMouseCallback("Image", draw_rectangle)
 
     # Attendre l'entrée utilisateur
-    print("[INFO] Tracez un rectangle autour de la zone d'intérêt, puis appuyez sur 'c' pour confirmer ou 'q' pour quitter.")
+    print("[INFO] Tracez des rectangles autour des zones d'intérêt.")
+    print("[INFO] Appuyez sur 'c' pour confirmer ou 'q' pour quitter.")
     while True:
         key = cv2.waitKey(1) & 0xFF
         if key == ord("c"):  # Confirmer
@@ -44,6 +44,34 @@ def annotate_image(image_path):
 
     cv2.destroyAllWindows()
     return roi_coords
+
+
+# Répertoires des images et annotations
+source_dir = "datasets/dataset/train_segment"
+image_dir = "test_annot/images/"  # Répertoire contenant les images
+output_dir = "test_annot/labels/"  # Répertoire pour sauvegarder les annotations
+NB_FICHIERS = 50 # Nombre d'images à annoter
+if os.path.exists(image_dir):
+    shutil.rmtree(image_dir)
+os.makedirs(image_dir)
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir)
+os.makedirs(output_dir)
+
+
+files = [f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))]
+random.shuffle(files)
+train_files = files[:NB_FICHIERS]
+
+# Fonction pour déplacer les fichiers
+def move_files(file_list, destination_dir):
+    for file in file_list:
+        shutil.copy(os.path.join(source_dir, file), os.path.join(destination_dir, file))
+
+# Déplacer les fichiers
+move_files(train_files, image_dir)
+
+compteur = 0
 
 # Parcourir les images
 for image_file in os.listdir(image_dir):
@@ -56,22 +84,28 @@ for image_file in os.listdir(image_dir):
         continue
 
     # Annoter l'image
-    roi = annotate_image(image_path)
+    rois = annotate_image(image_path)
     image = cv2.imread(image_path)
-    image = cv2.resize(image, (640,640))
-    if roi:
-        # Normaliser les coordonnées pour le format YOLO
-        x_min, y_min = roi[0]
-        x_max, y_max = roi[1]
-        x_center = (x_min + x_max) / 2 / image.shape[1]
-        y_center = (y_min + y_max) / 2 / image.shape[0]
-        width = (x_max - x_min) / image.shape[1]
-        height = (y_max - y_min) / image.shape[0]
-
-        # Sauvegarder les annotations
+    if rois:
         with open(output_path, "w") as f:
-            f.write(f"0 {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
-        print(f"[INFO] Annotation sauvegardée pour {image_file}.")
+            for roi in rois:
+
+                x_min = min(roi[0][0], roi[1][0])
+                y_min = min(roi[0][1], roi[1][1])
+                x_max = max(roi[0][0], roi[1][0])
+                y_max = max(roi[0][1], roi[1][1])
+            
+                # Normaliser les coordonnées pour le format YOLO
+                x_center = (x_min + x_max) / 2 / image.shape[1]
+                y_center = (y_min + y_max) / 2 / image.shape[0]
+                width = (x_max - x_min) / image.shape[1]
+                height = (y_max - y_min) / image.shape[0]
+
+                # Écrire les annotations
+                f.write(f"0 {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
+        print(f"[INFO] Annotations sauvegardées pour {image_file}.")
+        compteur += 1 
+        print(" NOMBRE D'IMAGES TRAITÉES : ", compteur)
     else:
         print(f"[INFO] Aucune annotation pour {image_file}.")
 
